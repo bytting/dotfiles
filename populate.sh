a523b0806777f63da3df6432d637cfe840813efe
Dag Robøle <dag.robole@gmail.com>
2015-12-25 20:38:30 +0100

    Some adjustments

--- populate.sh
+++ populate.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# Store path to dotfiles
 src=`cd $(dirname $0)/home && pwd`
 
+# Create folders under $HOME
 for target in `find $src -type d`
 do
     mkdir -p ${target/#$src/$HOME}
 done
 
+# Create symbolic links under $HOME
 for target in `find $src -type f`
 do
     ln -sf $target ${target/#$src/$HOME}
